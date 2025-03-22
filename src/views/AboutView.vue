71ed2dfe1e8259d088ba4355a3d8680dbaafa304
Alicha Zahra <alichaicha248@gmail.com>
2025-03-22 08:00:59 +0700

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -128,7 +128,7 @@ export default {
         </div>
         <div class="md:w-7/12">
           <p class="mb-3 md:mb-7 fadein-left fadeins-1">
-            &nbsp; &nbsp; &nbsp; Hi everyone! My name is Fadlan Syahid Ali. I'm a web developer from Bekasi, West Java. I
+            &nbsp; &nbsp; &nbsp; Hi everyone! My name is satoru.lia. I'm a web developer from Pekalongan, Central Java. I
             have 1 year of experience in back-end web development.
             I really enjoy what I do right now, in my opinion, creating programs is not just a job, but also an art that
             has
